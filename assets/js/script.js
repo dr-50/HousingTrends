c9089f99229b62e449397401f4fcd8a431b44d32
Devon <devonross50@yahoo.com>
2021-12-25 15:39:11 -0600

    functioning fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,13 +12,33 @@ function myFunction1(){
       }
     } 
 
+var API = key
 
 
 function eiaApiFetch(){
-  fetch('http://api.eia.gov/series/?series_id=NG.N3010WI2.M&api_key='&API&'&category_id=480302')
+  fetch(`http://api.eia.gov/series/?series_id=NG.N3010WI2.M&api_key=${API}&category_id=480302`)
   .then(response => response.json())
-  .then(data => data.series[0].data.forEach(element=>console.log(element)));
+  .then(data =>{
+    var output = data.series[0].data
+  
+    var prodMonth = []
+    var consumption = []
 
+    for (i=0; i <100; i++){
+      prodMonth.push(output[i][0]);
+    }
+    console.log(prodMonth)
+    
+    for (i=0; i<100; i++){
+      consumption.push(output[i][1])
+    }
+    console.log(consumption)
+
+    });
+    
+  // console.log(output[0][0])
+  //   })
+  }
   //push .data to variable 
   //create two array variables
   //push first value from each .data array into array variable 
@@ -26,4 +46,3 @@ function eiaApiFetch(){
   //data.serise[i].data.length-1 (last value) 
   //push into array 
   //}
-}
